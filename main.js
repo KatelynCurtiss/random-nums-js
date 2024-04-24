a85dfd2d38aca36e884850d8bb0c15c36fb8651e
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-04-24 10:22:40 -0400

    created html, js, and style.css folders

---
+++ main.js
@@ -0,0 +1,42 @@
+// Katelyn Curtiss
+// 24 APR 2024
+// Random Numbers PRIMM 
+
+
+//Input 1
+/*const randNum1 = Math.random();
+const randNum2 = Math.random();
+const randNum3 = Math.random();
+
+
+
+//Process
+
+
+
+
+//Output 1
+console.log(`First random numberis ${randNum2}`);
+console.log(`Second random numberis ${randNum2}`);
+console.log(`Third random numberis ${randNum3}`);*/
+
+//Input 2
+/*const firstRandNum = Math.random()*5;
+const secondRandNum = Math.random()*5;
+const thirdRandNum = Math.random()*5;
+
+
+//Output 2
+console.log(`The first random number is:${firstRandNum}`);
+console.log(`The second random number is:${secondRandNum}`);
+console.log(`The third random number is:${thirdRandNum}`);*/
+
+//input 3
+const random1 = Math.floor(Math.random()*5);
+const random2 = Math.floor(Math.random()*5);
+const random3 = Math.floor(Math.random()*5);
+
+// Output 3
+console.log(`The first random number is:${random1}`);
+console.log(`The second random number is:${random2}`);
+console.log(`The third random number is:${random3}`);
